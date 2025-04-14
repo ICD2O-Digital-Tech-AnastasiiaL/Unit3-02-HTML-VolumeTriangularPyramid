19aec95ea2525ff04f6001b6edd1fa15fc67d8c7
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-04-14 14:58:09 +0000

    Created first volume cal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the volume of the triangle
+ */
+function calculateVolume() {
+  // Information about values
+  let height = parseInt(document.getElementById('height-of-the-triangle').value);
+    let base = parseInt(document.getElementById('base-of-the-triangle').value);
+
+  // Calculation
+  let volume = (1/3) * base * height
+
+  // Display
+  document.getElementById('volume').innerHTML = 'Volume of a triangle is: ' + volume + ' cm^3'
+}
